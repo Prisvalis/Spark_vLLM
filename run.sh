3587f71e20cb210fa1240814cb9bfe8370729906
JW-Albert <ru04jo30801@gmail.com>
2026-09-19 21:54:10 +0800

    新增 tiktoken_encodings 目錄並下載必要的編碼檔案；更新 docker-compose.yml 檔案以包含相應的卷設定

--- run.sh
+++ run.sh
@@ -15,5 +15,11 @@ fi
 echo "Stopping any existing containers..."
 docker compose -f "$COMPOSE_FILE" --env-file "$ENV_FILE" down
 
+echo "Download Harmony..."
+cd docker
+mkdir -p tiktoken_encodings
+wget -O tiktoken_encodings/o200k_base.tiktoken "https://openaipublic.blob.core.windows.net/encodings/o200k_base.tiktoken"
+wget -O tiktoken_encodings/cl100k_base.tiktoken "https://openaipublic.blob.core.windows.net/encodings/cl100k_base.tiktoken"
+
 echo "Starting Docker environment..."
 docker compose -f "$COMPOSE_FILE" --env-file "$ENV_FILE" up -d
